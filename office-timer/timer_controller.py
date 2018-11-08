3dc246036fd71e124350786bad9df44aa373b57e
Aditya Kamble <adityakamble49@gmail.com>
2018-11-08 18:19:17 +0530

    Countdown timer added

---
+++ office-timer/timer_controller.py
@@ -0,0 +1,23 @@
+import time
+from optparse import OptionParser
+
+
+def build_option_parser():
+    parser = OptionParser()
+    parser.add_option("-t", "--time", dest="given_time", type="int")
+    return parser.parse_args()
+
+
+def countdown_timer(given_time_seconds):
+    while given_time_seconds:
+        minutes, seconds = divmod(given_time_seconds, 60)
+        time_format = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(time_format, end='\r')
+        time.sleep(1)
+        given_time_seconds -= 1
+
+
+if __name__ == '__main__':
+    (options, args) = build_option_parser()
+    given_time = options.given_time
+    countdown_timer(given_time)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='office-timer',
+    version='0.0.1',
+    packages=['office-timer'],
+    url='https://github.com/adityakamble49/office-timer',
+    license='Apache-2.0',
+    author='adityakamble49',
+    author_email='adityakamble49@gmail.com',
+    description='Office Timer - Manage work life balance'
+)
